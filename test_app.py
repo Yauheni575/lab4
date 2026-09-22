a45c844794c0de70ce7a6f7ad7a1218cb8980712
Xsirk <gkgekatop1gg@gmail.com>
2026-09-22 23:35:34 +0300

    Show example results in test output

--- test_app.py
+++ test_app.py
@@ -2,6 +2,8 @@ import pytest
 from app import add
 
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0)])
+def test_add(a, b, expected):
+    result = add(a, b)
+    print(f"add({a}, {b}) = {result}")
+    assert result == expected
