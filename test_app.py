745640796aa91aa4ea3eb053bdd83a2af676c429
Xsirk <gkgekatop1gg@gmail.com>
2026-09-22 23:36:57 +0300

    Revert test output changes

--- test_app.py
+++ test_app.py
@@ -2,8 +2,6 @@ import pytest
 from app import add
 
 
-@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (-1, 1, 0)])
-def test_add(a, b, expected):
-    result = add(a, b)
-    print(f"add({a}, {b}) = {result}")
-    assert result == expected
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
